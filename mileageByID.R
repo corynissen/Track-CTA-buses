022b40e6d361a001f86442d6a39f7685ee70902c
corynissen <corynissen@gmail.com>
2011-11-01 13:32:07 -0500

    first commit of mileageByID.R file

---
+++ mileageByID.R
@@ -0,0 +1,29 @@
+
+# code to return a list of IDs (buses) and their daily mileage
+
+require(geosphere)
+
+# run readData.R to create data/fixdata.rdata file
+# load data created in readData.R
+load("data/fixdata.rdata")
+
+getMileage <- function(data){
+  # data:  dataframe with longitude and latitude columns in numeric format
+  # output:  Sum of distances between lat-long pairs
+  # make sure data is still sorted by request time
+  data <- data[order(data$requesttime),]
+  # create a pair of lagged lat and long variables.
+  data$lag_long <- c(NA, data$longitude[1:dim(data)[1]-1])
+  data$lag_lat <- c(NA, data$latitude[1:dim(data)[1]-1])
+  # use distVincentyEllipsoid function to calculate the distances between
+  # each lat long pair and it's lagged lat long pair  
+  result <- distVincentyEllipsoid(cbind(data$latitude, data$longitude), cbind(data$lag_lat, data$lag_long))
+  # sum and convert meters to miles
+  result <- sum(result, na.rm=T) / 1609.344
+  return(result)
+}
+
+a <- by(fixdf, fixdf$id, getMileage)
+mileage <- data.frame(id=as.character(names(a)), miles=as.numeric(a))
+rm(a); gc()
+write.csv(mileage, "output/distance_by_id.csv", row.names=F)
