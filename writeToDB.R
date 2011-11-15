3f15507d3cbe1cd02bce2858b2fdc0437ab58c35
cory nissen <corynissen@gmail.com>
2011-11-15 03:13:24 +0000

    working to get clob upload working

--- writeToDB.R
+++ writeToDB.R
@@ -4,12 +4,12 @@ require(RODBC)
 # reformat dataframe to make it semi-colon delimited clob
 clob <- paste(mileage$id, mileage$miles, sep=",", collapse=";")
 
-# setup connect to XE, a unixODBC data source on my computer
+# setup connect to CTA, a unixODBC data source on my computer
 con <- odbcConnect("CTA")
 
 # append mileage df to tablename
 # will save response message to response variable
-response <- sqlQuery(con, call pg_cta.WriteAllBusMileage(ydate, clob))
+response <- sqlQuery(con, paste("call pg_cta.WriteAllBusMileage('", ydate, "', '", clob, "')", sep=""))
 
 # close the connection to avoid warnings at shutdown of R
 odbcClose(con)
