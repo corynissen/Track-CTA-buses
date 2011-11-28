e75a59c2c9718aae92f3f38c14b771cd6d503c7b
cory nissen <corynissen@gmail.com>
2011-11-27 20:32:35 -0600

    added shutdown command

--- go.R
+++ go.R
@@ -6,3 +6,5 @@ source("readData.R") #fetch yesterday's data, make fixdf data frame
 source("mileageByID.R") #calculate mileage by bus, make mileage data frame
 source("writeToDB.R") #write mileage to database
 
+# this runs on an EC2 instance...  This line will call a shutdown routine
+system("sudo /sbin/shutdown -h now")
