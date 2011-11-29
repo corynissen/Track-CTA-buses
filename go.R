abf6866cf056189778260a2002b0281d2fb45cf9
cory nissen <corynissen@gmail.com>
2011-11-28 20:32:54 -0600

    Updated path from relative to absolute

--- go.R
+++ go.R
@@ -2,9 +2,9 @@
 # This packages up the separate procedures to run in command line
 # R CMD BATCH --vanilla go.R
 
-source("readData.R") #fetch yesterday's data, make fixdf data frame
-source("mileageByID.R") #calculate mileage by bus, make mileage data frame
-source("writeToDB.R") #write mileage to database
+source("/src/git/Track-CTA-buses/readData.R") #fetch yesterday's data, make fixdf data frame
+source("/src/git/Track-CTA-buses/mileageByID.R") #calculate mileage by bus, make mileage data frame
+source("/src/git/Track-CTA-buses/writeToDB.R") #write mileage to database
 
 # this runs on an EC2 instance...  This line will call a shutdown routine
 system("sudo /sbin/shutdown -h now")
